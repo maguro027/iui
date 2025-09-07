07e3b419a4f34e1a94172ce90a6d1d07ef370acd
maguro027 <maguro027@gmail.com>
2025-09-08 03:13:03 +0900

    IUIItemUPLoadRequestクラスのリファクタリング: 不要なメソッドとフィールドを削除し、シンプルなコンストラクタに統合しました。

--- src/main/java/waterpunch/tool/server/packet/client/IUIItemUPLoadRequest.java
+++ src/main/java/waterpunch/tool/server/packet/client/IUIItemUPLoadRequest.java
@@ -4,63 +4,10 @@ import waterpunch.tool.item.IUIItem;
 
 public class IUIItemUPLoadRequest extends ClientPacket {
 
-     private final IUIItem item;
-
-     public IUIItemUPLoadRequest(IUIItem iuiItem) {
-          super(ClientPacketType.IUIItemUPLoadRequest);
-          this.item = iuiItem;
-     }
-}
-
-package waterpunch.tool.server.packet.client;
-
-import java.util.ArrayList;
-
-import waterpunch.tool.item.IUIItem;
-
-public class IUIItemUPLoadRequest extends ClientPacket {
-
-    private final ArrayList<IUIItem> items = new ArrayList<>();
-
-    public IUIItemUPLoadRequest() {
-        super(ClientPacketType.IUIItemUPLoadRequest);
-    }
+    private final IUIItem item;
 
     public IUIItemUPLoadRequest(IUIItem iuiItem) {
         super(ClientPacketType.IUIItemUPLoadRequest);
-        addItem(iuiItem);
-    }
-
-    public IUIItemUPLoadRequest(ArrayList<IUIItem> iuiItems) {
-        super(ClientPacketType.IUIItemUPLoadRequest);
-        items.addAll(iuiItems);
-    }
-
-    public final void addItem(IUIItem iuiItem) {
-        items.add(iuiItem);
-    }
-
-    public void addItems(ArrayList<IUIItem> iuiItems) {
-        items.addAll(iuiItems);
-    }
-
-    public IUIItem getItem(int index) {
-        return items.get(index);
-    }
-
-    public ArrayList<IUIItem> getItems() {
-        return items;
-    }
-
-    public void removeItem(int index) {
-        items.remove(index);
-    }
-
-    public void clearItems() {
-        items.clear();
-    }
-
-    public int size() {
-        return items.size();
+        this.item = iuiItem;
     }
 }
